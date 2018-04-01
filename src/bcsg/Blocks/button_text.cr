6da56b17149d6cccfd9938ab925ece46cca2acb3
spencer robinson <spencergrobinson@gmail.com>
2018-04-01 12:28:41 +0000

    added a button with text block

---
+++ src/bcsg/Blocks/button_text.cr
@@ -0,0 +1,13 @@
+
+class Button_Text < Button 
+  def initialize(strings, bools, name, parent = nil, properties = nil, gui = nil)
+    super
+    temp_bools   = {} of String => Bool
+    temp_strings = {} of String => String
+    @text = Text.new temp_strings, temp_bools, "label", nil, nil, get_gui
+    @text.content = "sample"
+    @text.parent = self
+    @text.dims = "50,35,100,60"
+    @children << @text
+  end 
+end 
